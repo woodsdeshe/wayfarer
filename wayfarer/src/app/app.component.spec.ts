e36c3175a653686258cfd82eb90e1f7001341ebf
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-24 12:14:48 -0700

    fixing last test

--- wayfarer/src/app/app.component.spec.ts
+++ wayfarer/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wayfarer app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('wayfarer app is running!');
   });
 });
