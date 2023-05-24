d884f11b52e3216accf65586df5fca58a98f838b
DeShe Woods <woodsdeshe94@gmail.com>
2023-05-24 15:44:01 -0400

    Title and Logo for Header complete

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
