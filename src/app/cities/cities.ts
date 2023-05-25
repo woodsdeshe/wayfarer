c53ca485d142390f9ca1cbb7139a6a179235162b
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-25 12:25:58 -0700

    fixed cities array to an array of objects

--- src/app/cities/cities.ts
+++ src/app/cities/cities.ts
@@ -2,7 +2,7 @@ interface Cities {
     id: number, 
     name: string,
     img: string, 
-    post: string[]
+    post: any[]
 
 }
 export const cities: Cities[] = [
@@ -10,25 +10,51 @@ export const cities: Cities[] = [
         id: 1,
         name: 'San Francisco',
         img: 'imagepath',
-        post: ["post1"]
+        post: [
+            {"title": "Post 1", 
+            "description": "this is the description for post 1!"}, 
+            {"title": "Post 2", 
+            "description": "this is the description for post 2!"}, 
+            {"title": "Post 3", 
+            "description": "this is the description for post 3!"}, 
+            {"title": "Post 4", 
+            "description": "this is the description for post 1!"}, 
+        ]
     },
     {
         id: 2,
         name: 'London',
         img: 'imagepath',
-        post: ["post2", "post3"]
+        post: [
+            {"title": "Post 5", 
+            "description": "this is the description for post 5!"}, 
+            {"title": "Post 6", 
+            "description": "this is the description for post 6!"}, 
+            {"title": "Post 7", 
+            "description": "this is the description for post 7!"}, 
+            {"title": "Post 8", 
+            "description": "this is the description for post 18"}, 
+        ]
     }, 
     {
         id: 3,
         name: 'Dar es Salaam',
         img: 'imagepath',
-        post: ["post4", "post5","post5"]
+        post: [
+            {"title": "Post 9", 
+            "description": "this is the description for post 9!"}
+        ]
     }, 
     {
         id: 4,
         name: 'Amsterdaam',
         img: 'imagepath',
-        post: ["post4", "post5"]
+        post: [
+            {"title": "Post 10", 
+            "description": "this is the description for post 10!"}, 
+            {"title": "Post 11", 
+            "description": "this is the description for post 11!"}
+        ]
     }
 
 ]
