304e1d16538e56b92e0f988925a50eafff25b15f
Dominique <DCWTLT1@gmail.com>
2023-05-25 16:16:12 -0500

    updates of search bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  canSeeSearch: boolean = false;
+  toggleSearchBar() {
+      this.canSeeSearch = false 
+  }
+  searchBack() {
+    this.canSeeSearch = true;
+  }
 }
